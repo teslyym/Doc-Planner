0ce4f78778a88cf0b6c9ecfbfb347bfcc156615b
Teslim <steslimolayiwola@gmail.com>
2024-03-11 15:46:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,384 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body>
+        <header>
+            <img src="./res/asset 0.png" width="300px" />
+            <nav>
+                <a id= "selected" href="">About Us</a>
+                <a href="">Career</a>
+                <a href="">Department</a>
+            </nav>
+
+        </header>
+    <section id="class">
+        <img src="./res/logo.png" alt="" width="3%"/>
+        <h1>Making the healthcare experience more human</h1>
+        <div id="twos">
+            <p class="text">We want patients to find the perfect doctor and book an appointment in the most easy way. The patient journey should be enjoyable, and that's why we are always next to them: to help them find the best possible care. Anytime, anywhere.</p>
+            <p class="text">We also help doctors to better manage their practice and build their online reputation. With our integrated end-to-end solution, doctors are able not only to improve their online presence, but also to devote their time to what really matters: their patients.</p>
+        </div>
+    </section>
+    <section>
+        <div id="blocks">
+            <div id="block1">
+            <img src="./res/asset 2.svg" alt="picture"/>
+            <h1>For patients</h1> 
+                <p>
+                Find a doctor, book a <br/> visit and solve any  <br/>health-related doubt
+
+            </p>
+            <div id="block1a">
+                <form action="/action_page.html">
+                    <select id="Country" name="Country" >
+                        <option value="Argentina">Choose a Country</option>
+                        <option value="Argentina">Argentina</option>
+                        <option value="Brazil">Brazil</option>
+                        <option value="Chile">Chile</option>
+                        <option value="Columbia">Columbia</option>
+                        <option value="Czech">Czech</option>
+                        <option value="Germany">Germany</option>
+                        <option value="Italy">Italy</option>
+                        <option value="Mexico">Mexico</option>
+                        <option value="Peru">Peru</option>
+                        <option value="Poland">Poland</option>
+                        <option value="Portugal">Portugal</option>
+                        <option value="Spain">Spain</option>
+                        <option value="Turkey">Turkey</option>
+                    </select>
+                </form>
+
+            </div>
+
+            </div>
+            
+            <div id="block2">
+                <img src="./res/asset 3.svg" alt="picture"/>
+               <h1>For doctors</h1>
+                <p>
+                    Save time managing visits and cut no-shows by half
+
+                </p>
+                <div id="block2a">
+                    <form action="/action_page.html">
+                        <select id="Country" name="Country" >
+                            <option value="Argentina">Choose a Country</option>
+                            <option value="Argentina">Argentina</option>
+                            <option value="Brazil">Brazil</option>
+                            <option value="Chile">Chile</option>
+                            <option value="Columbia">Columbia</option>
+                            <option value="Czech">Czech</option>
+                            <option value="Germany">Germany</option>
+                            <option value="Italy">Italy</option>
+                            <option value="Mexico">Mexico</option>
+                            <option value="Peru">Peru</option>
+                            <option value="Poland">Poland</option>
+                            <option value="Portugal">Portugal</option>
+                            <option value="Spain">Spain</option>
+                            <option value="Turkey">Turkey</option>
+                        </select>
+                    </form>
+    
+                </div>
+    
+
+
+            </div>
+            <div id="block3">
+                <img src="./res/asset 4.svg" alt="" />
+                <h1>For Clinics</h1>
+                <p>
+                    Deliver an exceptional patient experience in your clinic
+
+                    <a href="">2 to 20 specialist</a><br/><a href="">More than 20 speacialist</a>
+                </p>
+              
+        </div>
+        </div>
+    </section>
+    <section id="logos"> <div>
+        <p class="text">We are a global <br/> company with <br/> local culture</p>
+    </div>
+    <div class="difflogos">
+        <img src="./res/asset 5.png" width="100px" alt=""/>
+        <img src="./res/asset 6.png" alt="100px"/>
+        <img src="./res/asset 7.png" alt="100px"/>
+        <img src="./res/asset 8.png" alt="100px"/>
+        <img src="./res/asset 9.jpeg" alt="100px"/>
+        <img src="./res/asset 10.svg" alt="100px"/>
+        <img src="./res/asset 11.svg" alt="100px"/>
+        <img src="./res/asset 12.svg" alt="100px"/>
+        <img src="./res/asset 13.svg" alt="100px"/>
+    </div>
+        
+    </section>
+
+    {/* <!-- <section id="section3">
+        <div class="largelogo">
+        
+        <div id="textlargelogo">
+            <h1>The world's biggest <br> healthcare platform </h1>
+    
+            <p>We work from 9 offices all over the world, bringing Docplanner <br> Group to life in 13 countries.</p>
+
+         </div>
+         <div id="imglargelogo"> 
+            <img src="./res/asset 15.png" alt="" width="400">
+        </div>
+        </div> 
+
+        <div class="inboxes">
+            <div class="box1">
+                <div class="box1a">
+                    <i class="flag"></i>
+                    <h1>Leader in <br> 13 countries</h1>
+                    <p>Poland, Brazil, Mexico, Spain, Italy, Germany, Turkey, Colombia, Czech Republic, Portugal, Argentina, Peru and Chile</p>
+
+                </div>
+                <div class="box2">
+                    <i class="box1b"></i>
+                    <h1>90,000,000 <br> patients</h1>
+                    <p>visit our websites each month</p>
+                        
+                </div>
+            </div>
+            <div class="leftcon">
+
+                <div class="box3">
+                    <i class="box1c"></i>
+                    <h1>15,000,000 <br> appointments</h1>
+                    <p>booked last month</p>
+
+                </div>
+
+
+
+                <div class="box4">
+                    <i class="box1d"></i>
+                    <h1>
+                        210,000 active <br> doctors
+                    </h1>
+                    <p>trust our solutions</p>
+
+                </div>
+            </div>
+                
+
+               
+            </div> -->
+
+        
+    <!-- </div> -->
+    <!-- </section> --> */}
+    <section>
+        <div id="large">
+            <div id="largelogo">
+            <h1>The world's biggest<br/> healthcare platform</h1>
+            <p>We work from 9 offices all over the world, bringing Docplanner <br/>Group to life in 13 countries.</p>
+            
+            <img src="./res/asset 15.png" alt=""/>
+          
+        </div>
+           
+           
+        <div id="fourboxes">
+                <div class="twobox" id="firstb">
+                    <div class="innerbox">
+                        <img src="./res/asset 16.png" alt=""/>
+                        <img src="" alt=""/>
+                        <h1>Leader in 13 countries</h1>
+                        <p>Poland, Brazil, Mexico, Spain, Italy, Germany, Turkey, Colombia, Czech Republic, Portugal, Argentina, Peru and Chile</p>
+                    </div>
+                    <div class="innerbox">
+                        <img src="./res/asset 17.png" alt=""/>
+                        <img src="" alt=""/>
+                        <h1>90,000,000 <br/> patients</h1>
+                        <p>visit our websites each month</p>
+                    </div>
+
+                </div>
+                <div class="twobox">
+                    <div class="innerbox">
+                        <img src="./res/asset 18.png" alt=""/>
+                        <img src="" alt=""/>
+                        <h1>15,000,000 appointments</h1>
+                        <p>booked last month</p>
+                    </div>
+                    <div class="innerbox">
+                        <img src="./res/asset 19.png" alt=""/>
+                        <h1>210,000 active doctors</h1>
+                        <p>trust our solutions</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        {/* </div> */}
+    {/* </div> */}
+    </section>
+    {/* <!-- <section>
+        <div class="Section4">
+            <h1>Improve the lives of millions.</h1>
+            <h1>Change yours forever.</h1>
+            <p>More than 2800 team mates share our same vision, goals and passion. <br> With offices in Warsaw, Barcelona, Munich, Istanbul, Rome, Rio, Bologna,<br> Mexico City and Curitiba, our search for great talent never stops.</p>
+        </div>
+
+        <div id="bigimg">
+            <div class="pictures">
+                <img src="./res/asset 20.png" alt="">
+                <div class="pcaption">
+                    <p>Warsaw</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="pictures">
+                <img src="./res/asset 21.png" alt=""/>
+                <div class="pcaption">
+                    <p>BARCELONA</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="pictures">
+                <img src="./res/asset 22.png" alt=""/>
+                <div class="pcaption">
+                    <p>MUNICH</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="pictures">
+                <img src="./res/asset 23.png" alt=""/>
+                <div class="pcaption">
+                    <p>INSTANBUL</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="pictures">
+                <img src="./res/asset 24.png" alt=""/>
+                <div class="pcaption">
+                    <p>ROME</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="pictures">
+                <img src="./res/asset 25.png" alt=""/>
+                <div class="pcaption">
+                    <p>BOLOGNA</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="pictures">
+                <img src="./res/asset 26.png" alt=""/>
+                <div class="pcaption">
+                    <p>CURITIBA</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="pictures">
+                <img src="./res/asset 27.png" alt=""/>
+                <div class="pcaption">
+                    <p>Rio De Janeiro</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="pictures">
+                <img src="./res/asset 28.png" alt=""/>
+                <div class="pcaption">
+                    <p>Mexico City</p>
+                    <p class="pc">SEE OPENINGS</p>
+                </div>
+            </div> --> */}
+
+
+        {/* <!-- </div> --> */}
+        <section id="section6">
+            <div class="classimgs">
+                <img src="./res/asset 20.png" alt=""/>
+                <div>
+                    <p class="COT">Warsaw</p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="classimgs">
+                <img src="./res/asset 21.png" alt=""/>
+                <div>
+                    <p class="COT">Barcelona</p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="classimgs">
+                <img src="./res/asset 22.png" alt=""/>
+                <div>
+                    <p class="COT">Munich</p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="classimgs">
+                <img src="./res/asset 23.png" alt=""/>
+                <div>
+                    <p class="COT">Istanbul</p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="classimgs">
+                <img src="./res/asset 24.png" alt=""/>
+                <div>
+                    <p class="COT">Rome</p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="classimgs">
+                <img src="./res/asset 25.png" alt=""/>
+                <div>
+                    <p class="COT">Bologna </p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="classimgs">
+                <img src="./res/asset 26.png" alt=""/>
+                <div>
+                    <p class="COT">Curitba</p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="classimgs">
+                <img src="./res/asset 27.png" alt=""/>
+                <div>
+                    <p class="COT">Rio De Janeiro</p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+            <div class="classimgs">
+                <img src="./res/asset 28.png" alt=""/>
+                <div>
+                    <p class="COT">Mexico</p>
+                    <p id="but">SEE OPENINGS</p>
+                </div>
+            </div>
+    </section>
+    <section id="section7">
+        <p id="sec7a">Sounds interesting? Join us now!</p>
+        <p id="sec7b">SEE ALL CURRENT OPENINGS</p>
+    </section>
+    <footer>
+        <p id="footer1">
+            We are leaders in 13 countries: <span> <a href="">Poland</a>, <a href="">Turkey</a> , <a href="">Spain</a> , <a href="">Italy</a> , <a href="">Germany</a> , <a href="">Czech Republic</a> , <a href="">Portugal</a> , <a href="">Mexico</a> , <a href="">Colombia</a> , <a href="">Brazil</a> , <a href="">Argentina</a> , <a href="">Peru</a> and <a href="">Chile</a></span>
+        </p>
+        <p id="footer2">
+            This site uses cookies to deliver services in accordance with this Privacy Policy. You can specify the conditions for storing or accessing cookies on your browser.
+
+        </p>
+        <p>
+            www.docplanner.com © 2023
+
+        </p>
+    </footer>
+</body>
+    </>
+  )
+}
+
+export default App
